ffd6e91196331549ed79f1b4ef7bce9116ac3d44
Andrew Grzeszczak <andrew.s.grzeszczak@gmail.com>
2026-03-23 08:48:42 -0500

    chore: spawn crates

---
+++ glass-cli/src/main.rs
@@ -0,0 +1,14 @@
+
+fn print_header() {
+    println!();
+    println!("~~~~~~~~~~~~~~~~~~");
+    println!("|                |");
+    println!("| Glass Compiler |");
+    println!("|                |");
+    println!("~~~~~~~~~~~~~~~~~~");
+    println!();
+}
+
+fn main() {
+    print_header();
+}
